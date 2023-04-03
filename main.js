4c19b2bdfa2fe45da3cd533c5131cb52e8aa0b96
bigman <terrence.chebii@student.moringaschool.com>
2023-04-03 16:11:09 +0300

    finally done

--- main.js
+++ main.js
@@ -1,59 +1,55 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    screenWidth = screen.width.toString()
+    let div = document.querySelector('div')
+    div.style.width = screenWidth+'px'
+})
 
 
-fetch('http://localhost:4000/films')
-.then((res) => res.json())
-.then((data) => displayContent(data))
-
-
-function displayContent(content){
-    content.forEach(films => {
+function displayContent(films){
         let main = document.querySelector('main')
-        let list = document.createElement('li')
-        list.innerHTML = `
+        main.innerHTML = `
         <img id='${films.id}'src='${films.poster}'>
-        <p id='items'>Available tickets${films.capacity}<br>
+        <p id='items'>Available tickets : ${films.capacity - films.tickets_sold}<br>
         screentime :${films.showtime}<br>
         Duration: ${films.runtime}<br>
         <button id='button'>Buy Ticket</button>
         <button id='delete'>DELETE</button>
+        <h4>${films.description}</h4>
+
         `
-        main.appendChild(list)
-        let btn = list.querySelector('#button')
-        btn.addEventListener('click' , buyTicket)
-        let deleteMovie = list.querySelector('#delete')
+        let btn = main.querySelector('#button')
+        btn.onclick = (event) => {
+            event.preventDefault();
+            if(films.capacity > 0){
+            let remainingTickets = films.capacity  - 1
+            let soldTickets = films.tickets_sold + 1
+            let final = {tickets_sold : soldTickets}        
+            
+            fetch(`http://localhost:4000/films/${films.id}` , {
+                method : 'PATCH',
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify(final)
+            })
+        }else{
+            let soldOut = {
+                capacity : 'SOLD OUT !'
+            }
+    
+            fetch(`http://localhost:4000/films/${films.id}` , {
+                method : 'PATCH',
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify(soldOut)
+            })
+        }
+    }
+        let deleteMovie = main.querySelector('#delete')
         deleteMovie.addEventListener('click' , deleteIt)
           
 
-    function buyTicket(){
-        if(films.capacity > 0){
-        let remainingTickets = films.capacity  - 1
-        let soldTickets = films.tickets_sold + 1
-        let final = {
-            capacity : remainingTickets,
-            tickets_sold : soldTickets
-        }
-        
-        fetch(`http://localhost:4000/films/${films.id}` , {
-            method : 'PATCH',
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            body : JSON.stringify(final)
-        })
-    }else{
-        let soldOut = {
-            capacity : 'SOLD OUT !'
-        }
-
-        fetch(`http://localhost:4000/films/${films.id}` , {
-            method : 'PATCH',
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            body : JSON.stringify(soldOut)
-        })
-    }
-}
     function deleteIt(){
         fetch(`http://localhost:4000/films/${films.id}` , {
             method : 'DELETE',
@@ -62,20 +58,26 @@ function displayContent(content){
             },
         })
     }
-    fetch('http://localhost:4000/films/')
+    
+}
+
+fetch('http://localhost:4000/films/')
 .then((res) => res.json())
 .then((data) => displayNames(data))
 
-    function displayNames(names){
-          let menu = document.querySelector('#list')
-          let display =document.createElement('li')
-          display.innerHTML = `
-          <a href='#${films.id}'>${films.title}</a>
-          `
-          menu.appendChild(display)
+function displayNames(names){
+    names.forEach(films => {
+    let menu = document.querySelector('#list')
+    let display =document.createElement('li')
+    display.innerHTML = `
+    <p>${films.title}</p>
+    `
+    menu.appendChild(display)
+    display.onclick = () => {
+        displayContent(films)
     }
+
 })
 }
-
 
 
